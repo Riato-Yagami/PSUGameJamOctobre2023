5fa12fae445478126f3220e63295d99042342ff0
Jules T460 <jules.pesin@gmail.com>
2023-10-28 06:28:51 +0200

    gameee

--- scripts/manager/input_manager.gd
+++ scripts/manager/input_manager.gd
@@ -7,14 +7,18 @@ func set_player(new_player):
 	player = new_player
 			
 func _input(event):
-	for finger in fingers:
-		if event.is_action_pressed(finger):
-			player.unfold(finger)
-		if event.is_action_released(finger):
-			player.fold(finger)
-			
 	if event.is_action_released("start"):
 		SceneManager.load_game()
 			
 	if event.is_action_released("escape"):
 		SceneManager.quit()
+		
+	if(GameManager.in_game):
+		finger_input(event)
+		
+func finger_input(event):
+	for finger in fingers:
+		if event.is_action_pressed(finger):
+			player.unfold(finger)
+		if event.is_action_released(finger):
+			player.fold(finger)
